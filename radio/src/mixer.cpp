ba655f190b0bf83bbb6793502e3ec0d45386e80b
Woz4tetra <woz4tetra@gmail.com>
2026-03-14 16:42:31 -0400

    add new target. Fix override function

--- radio/src/mixer.cpp
+++ radio/src/mixer.cpp
@@ -488,7 +488,8 @@ void evalInputs(uint8_t mode)
         v = 0;
       }
 
-      if (mode <= e_perout_mode_inactive_flight_mode && isTrainerInputValid()) {
+      if (mode <= e_perout_mode_inactive_flight_mode && isTrainerInputValid() &&
+          isFunctionActive(FUNCTION_TRAINER_STICK1 + ch)) {
         // trainer mode
         TrainerMix* td = &g_eeGeneral.trainer.mix[ch];
         if (td->mode) {
